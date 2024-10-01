abe752a2f15bf38da1824617892293d54f75511f
NecheHephzibah <neche.bless@gmail.com>
2024-10-01 15:23:07 +0000

    corrected eslint errors in 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,12 @@
-export const taskFirst = () => { 
-	const task = "I prefer const when I can.";
-	return task;
+export const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
 };
 
-export const getLast = () => {
-	return "is okay";
-};
+export const getLast = () => 'is okay';
 
 export const taskNext = () => {
-	let combination = "But sometimes let ";
-	combination += getLast();
-	return combination;
+  let combination = 'But sometimes let ';
+  combination += getLast();
+  return combination;
 };

17f33d4fdd6c8ea4f41448af487a6bc98acf120e
NecheHephzibah <neche.bless@gmail.com>
2024-10-23 11:04:17 +0000

    Await / Async

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((resp) => {
-    const arr = [];
-    for (const item of resp) {
-      arr.push({ status: item.status, resp: item.resp || item.reason });
-    }
-    return arr;
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => (
+      values.map((item) => ({
+        status: item.status,
+        value: item.status === 'fulfilled' ? item.value : String(item.reason),
+      }))
+    ));
 }

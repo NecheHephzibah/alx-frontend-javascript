06f9724b059bd10ff2de61a3db818e786d1d1348
NecheHephzibah <neche.bless@gmail.com>
2024-10-23 10:37:15 +0000

    Await / Async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-    
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((signup) => {
+      console.log(`${signup[0].body} ${signup[1].firstName} ${signup[1].lastName}`);
+    })
+
+    .catch(() => console.log('Signup system offline'));
 }
